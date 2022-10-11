f8cc217cf42ce97dbd995db22a3b710990ba047b
Serhii <stupanenko.serhii@gmail.com>
2022-10-12 02:04:49 +0300

    fix

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const URL =
   document.domain === "localhost"
-    ? "http://localhost:5555"
+    ? "http://localhost:4444"
     : process.env.REACT_APP_API_URL;
 
 const instance = axios.create({
