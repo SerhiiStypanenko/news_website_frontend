90036130557fe8011b1bc28fcdbe49d1ad1e956b
Serhii <stupanenko.serhii@gmail.com>
2022-10-12 11:00:38 +0300

    fix

--- src/axios.js
+++ src/axios.js
@@ -1,16 +1,12 @@
 import axios from "axios";
 
-const URL = process.env.REACT_APP_API_URL;
-
 const instance = axios.create({
-  baseURL: "https://websitee-practice.herokuapp.com",
+    baseURL: process.env.REACT_APP_API_URL,
 });
 
 instance.interceptors.request.use((config) => {
-  config.headers.Authorization = window.localStorage.getItem("token");
-  return config;
+    config.headers.Authorization = window.localStorage.getItem('token');
+    return config;
 });
 
 export default instance;
-
-export { URL };
